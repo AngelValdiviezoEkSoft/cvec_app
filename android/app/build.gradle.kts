023526a2254c2e6298afd568c490b354e621a8f3
AngelValdiviezoEkSoft <angelvaldiviezo@ekuasoft.com>
2025-09-07 22:23:52 -0500

    Configuración para generación de apk y enviar a la play store

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,7 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -5,10 +9,25 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
     namespace = "com.example.cve_app"
     compileSdk = 35
     ndkVersion = flutter.ndkVersion
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -23,17 +42,19 @@ android {
         applicationId = "com.example.cve_app"
         minSdk = 21
         targetSdk = 35
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 1
+        versionName = "0.0.1"
     }
 
     buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             isShrinkResources = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
         }
     }
+
 }
 
 flutter {
---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,14 @@
+# Rules for Google ML Kit
+-keep class com.google.mlkit.** { *; }
+-keep class com.google.android.gms.internal.mlkit_vision_text_common.** { *; }
+-keep class com.google.android.gms.internal.mlkit_text_recognition.** { *; }
+-keep class com.google.android.gms.internal.mlkit_vision_barcode.** { *; }
+-keep class com.google.android.gms.internal.mlkit_vision_face.** { *; }
+-keep class com.google.android.gms.internal.mlkit_vision_label_image.** { *; }
+-keep class com.google.android.gms.internal.mlkit_vision_pose.** { *; }
+-keep class com.google.android.gms.internal.mlkit_vision_object_detection.** { *; }
+-keep class com.google.android.gms.internal.mlkit_vision_barcode_common.** { *; }
+
+# For GMS/Firebase libraries
+-keep class com.google.firebase.** { *; }
+-keep class com.google.android.gms.** { *; }
